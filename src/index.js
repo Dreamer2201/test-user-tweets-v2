5a0161626ede42c90e5f2b438f4df5b0c9a6c851
Oksana Polyakova <ddreamer22011986@gmail.com>
2023-05-20 22:15:11 +0300

    CHANGED: BrowserRouter on HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import './index.css';
 import App from './components/App/App';
@@ -11,13 +11,13 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<HashRouter>
 			<Provider store={store}>
 				<PersistGate persistor={persistor}>
 					<App />
 				</PersistGate>
 			</Provider>
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
 
